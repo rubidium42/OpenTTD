369e66c3aadfa2ac9f4465c7a8e5a86714aa14a6
Rubidium <rubidium@openttd.org>
2025-01-31 21:30:17 +0100

    Codechange: strongly type EngineRenew

--- src/saveload/saveload.cpp
+++ src/saveload/saveload.cpp
@@ -1119,7 +1119,7 @@ static size_t ReferenceToInt(const void *obj, SLRefType rt)
 		case REF_TOWN:      return ((const     Town*)obj)->index + 1;
 		case REF_ORDER:     return ((const    Order*)obj)->index + 1;
 		case REF_ROADSTOPS: return ((const RoadStop*)obj)->index + 1;
-		case REF_ENGINE_RENEWS:  return ((const       EngineRenew*)obj)->index + 1;
+		case REF_ENGINE_RENEWS:  return ((const       EngineRenew*)obj)->index.base() + 1;
 		case REF_CARGO_PACKET:   return ((const       CargoPacket*)obj)->index.base() + 1;
 		case REF_ORDERLIST:      return ((const         OrderList*)obj)->index + 1;
 		case REF_STORAGE:        return ((const PersistentStorage*)obj)->index + 1;
